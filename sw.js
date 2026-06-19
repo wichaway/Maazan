06b5328445f696110a255bc48bbb767e499757e9
wichaway <eliron29@gmail.com>
2026-06-19 19:18:53 +0300

    Add Firebase sync, Google login, PWA manifest

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'maazan-v1';
+const ASSETS = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
