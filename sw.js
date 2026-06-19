f607e2b6f8b01c94ba0985fd08bbc791dfe2d11f
wichaway <eliron29@gmail.com>
2026-06-19 22:13:05 +0300

    Unregister service worker to clear all caches

--- sw.js
+++ sw.js
@@ -1,4 +1,7 @@
-// Service worker - pass through all requests, no caching
-self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request));
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.registration.unregister())
+  );
 });
